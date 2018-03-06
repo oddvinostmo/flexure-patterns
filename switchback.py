98265dfbee5fe9540c781585846cccef75956e01
Oddvin <oddvinostmo@gmail.com>
2018-03-06 09:57:45 +0100

    modified switchback to become general

--- switchback.py
+++ switchback.py
@@ -8,14 +8,17 @@ import matplotlib.pyplot as plt
 import shapely.geometry
 import shapely.affinity
 import shapely.ops
+import patternGenerators as gen
 
-def sb_ext_square(cut_width, flexure_width, junction_length, edge_space, num_flex):
+def make_square_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default'):
     """
     """
     import numpy as np
     a = cut_width; b = flexure_width; c = junction_length; d = edge_space
-    sqrt2 = 2**0.5
-    ax = a/sqrt2/2 # x displacement along diagonal cut
+    if side_cut == 'default': # x displacement along diagonal cut
+        ax = cut_width/(2**0.5)/2    
+    else:
+        ax = side_cut
     dx = a+b # displacement y direction
     dy = dx # displacement y direction
     h0 = a+b/2+c # height in triangle
@@ -74,6 +77,62 @@ def sb_ext_square(cut_width, flexure_width, junction_length, edge_space, num_fle
     coords = [(x[i],y[i]) for i in range(len(x))]
     return shapely.geometry.Polygon(coords)
 
+def make_triangular_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default'):
+    """
+    Scales the square quart with 3**0.5 to match 30-120-30 triangle. addects edge_space and side_cut
+    """
+    xfact = 1.00001*3**0.5 # added tolerance for scaling
+    edge_space /= xfact
+    if side_cut == 'default':
+        side_cut = cut_width/xfact / (2**0.5/2)
+    sb_square_mod = make_square_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut)
+    return shapely.affinity.scale(geom=sb_square_mod, xfact=xfact)
 
-sb = sb_ext_square(cut_width=1, flexure_width=2, junction_length=5, edge_space=2, num_flex=4)
-print(sb)
+def make_hexagonal_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default'):
+    """
+    Scales the square quart with 1/3**0.5 to match 60-60-60 triangle. addects edge_space and side_cut
+    """
+    xfact = 1.00001/3**0.5 # added tolerance for scaling
+    edge_space /= xfact
+    if side_cut == 'default':
+        side_cut = cut_width/xfact /(2**0.5/2)
+    sb_square_mod = make_square_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default')
+    return shapely.affinity.scale(geom=sb_square_mod, xfact=xfact)
+
+"""
+Tiles
+"""
+
+def make_triangular_switchback_tile(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default'):
+    sb_third = make_triangular_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut)
+    xmin, ymin, xmax, ycoord = sb_third.bounds
+    xcoord = (xmax+xmin)/2
+    sb_third_r120 = shapely.affinity.rotate(geom=sb_third, angle=120, origin=(xcoord,ycoord))
+    sb_third_r240 = shapely.affinity.rotate(geom=sb_third, angle=240, origin=(xcoord,ycoord))
+    return shapely.ops.cascaded_union([sb_third, sb_third_r120, sb_third_r240])
+
+def make_square_switchback_tile(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default'):
+    sb_quart = make_square_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut)
+    xmin, ymin, xmax, ycoord = sb_quart.bounds # ycoord is ymax
+    xcoord = (xmax+xmin)/2
+    sb_quart_r90 = shapely.affinity.rotate(geom=sb_quart, angle=90, origin=(xcoord,ycoord))
+    sb_quart_r180 = shapely.affinity.rotate(geom=sb_quart, angle=180, origin=(xcoord,ycoord))
+    sb_quart_r270 = shapely.affinity.rotate(geom=sb_quart, angle=270, origin=(xcoord,ycoord))
+    return shapely.ops.cascaded_union([sb_quart, sb_quart_r90, sb_quart_r180, sb_quart_r270])
+
+def make_hexagonal_switchback_tile(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut='default'):
+    sb_sixt = make_hexagonal_switchback_gen_reg(cut_width, flexure_width, junction_length, edge_space, num_flex, side_cut)
+    xmin, ymin, xmax, ycoord = sb_sixt.bounds
+    xcoord = (xmax+xmin)/2
+    sb_rotated = [sb_sixt]
+    for n in range(6):
+        sb_rotated.append(shapely.affinity.rotate(geom=sb_sixt, angle=60*(1*n), origin=(xcoord,ycoord)))
+    return shapely.ops.cascaded_union(sb_rotated)
+
+
+
+
+#gen.plotPolygon(make_triangular_switchback_gen_reg(cut_width=1, flexure_width=2, junction_length=5, edge_space=3, num_flex=4, side_cut=1))
+#gen.plotPolygon(make_triangular_switchback_tile(cut_width=1, flexure_width=2, junction_length=5, edge_space=3, num_flex=2, side_cut=1))
+#gen.plotPolygon(make_hexagonal_switchback_gen_reg(cut_width=1, flexure_width=2, junction_length=5, edge_space=3, num_flex=4, side_cut=1))
+gen.plotPolygon(make_hexagonal_switchback_tile(cut_width=1, flexure_width=2, junction_length=5, edge_space=3, num_flex=5, side_cut='default'))
